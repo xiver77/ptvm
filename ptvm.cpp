e7594f63e07de8809c1c4e80e63015f2218ee530
xiver77 <xiver77@gmail.com>
2015-07-07 02:50:22 +0900

    Create ptvm.cpp

---
+++ ptvm.cpp
@@ -0,0 +1,12 @@
+#include "ptvm.hpp"
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    Error::abort("must have 1 argument");
+  }
+	std::vector<unsigned char> test = Interpreter::fileToVector(argv[1]);
+	Interpreter().start(test.begin(), test.end());
+	return EXIT_SUCCESS;
+}
